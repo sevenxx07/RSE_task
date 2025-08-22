83f541e1b21bd67453e6b24c8e3e62828a6d9823
sevenxx07 <saravesela01@gmail.com>
2025-08-22 12:36:12 +0200

    [3.subtask] - rewriting FCS pipeline with python

--- Snakefile
+++ Snakefile
@@ -1,8 +1,16 @@
+config = {
+    "script_type": "python",  # "python" or "R"
+    "use_channels": True      # True or False
+}
+
 import os, glob
+
 RAW_DIR = config.get('raw_dir', 'data/raw')
-PROCESSED_DIR = config.get('processed_dir', 'data/processed_channels')
-PLOT_DIR = config.get('plot_dir', 'plots_channels')
+PROCESSED_DIR = config.get('processed_dir', 'data/processed_channels_py')
+PLOT_DIR = config.get('plot_dir', 'plots_channels_py')
 CHANNELS_FILE = config.get('channels_file', 'channels.txt')
+SCRIPT_TYPE = config["script_type"]
+USE_CHANNELS = config["use_channels"]
 
 samples = [os.path.splitext(os.path.basename(f))[0] for f in glob.glob(os.path.join(RAW_DIR, '*.fcs'))]
 
@@ -18,6 +26,18 @@ rule process_fcs:
         fcs=os.path.join(PROCESSED_DIR, '{sample}_umap_clust.fcs'),
         plot=os.path.join(PLOT_DIR, '{sample}.png')
     params:
-        channels_file=CHANNELS_FILE
+        channels_flag=lambda wildcards: f"-c {CHANNELS_FILE}" if USE_CHANNELS else "",
+        script_type=SCRIPT_TYPE
     shell:
-        'Rscript {workflow.basedir}/scripts/process_fcs.R -i "{input}" -o "{output.fcs}" -p "{output.plot}" -c "{params.channels_file}"'
+        """
+        if [ "{params.script_type}" = "python" ]; then
+            python {workflow.basedir}/scripts/process_fcs.py \
+                -i "{input}" -o "{output.fcs}" -p "{output.plot}" {params.channels_flag}
+        else
+            Rscript {workflow.basedir}/scripts/process_fcs.R \
+                -i "{input}" -o "{output.fcs}" -p "{output.plot}" {params.channels_flag}
+        fi
+        
+        """
+        
+        #'Rscript {workflow.basedir}/scripts/process_fcs.R -i "{input}" -o "{output.fcs}" -p "{output.plot}" -c "{params.channels_file}"'
---
+++ scripts/process_fcs.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+import pandas as pd
+import umap
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from FlowCal.io import FCSData
+from fcswrite import write_fcs
+
+def main():
+    parser = argparse.ArgumentParser(description="Process FCS file")
+    parser.add_argument("-i", "--input", required=True, help="Input FCS file")
+    parser.add_argument("-o", "--output", required=True, help="Output FCS file")
+    parser.add_argument("-p", "--plot", required=True, help="Output plot file")
+    parser.add_argument("-c", "--channels", default=None, help="Optional channels.txt")
+    args = parser.parse_args()
+
+    ff = FCSData(args.input)
+    expr = np.array(ff, dtype=float)
+
+    if args.channels:
+        channels_df = pd.read_csv(args.channels, sep="\t")
+        selected = channels_df.loc[channels_df["use"] == 1, "desc"].tolist()
+        available_channels = ff.channel_labels()
+        present = [ch for ch in selected if ch in available_channels]
+        indices = [available_channels.index(ch) for ch in present]
+    else:
+        available_channels = ff.channel_labels()
+        indices = [i for i, desc in enumerate(available_channels) if
+                   desc and not any(s in desc.upper() for s in ["FSC", "SSC"])]
+
+    X = expr[:, indices]
+    X_asinh = np.arcsinh(X / 5)
+
+    umap_res = umap.UMAP(n_components=2).fit_transform(X_asinh)
+
+    km = KMeans(n_clusters=5, random_state=42).fit(X_asinh)
+
+    new_data = np.hstack([expr, umap_res, km.labels_[:, None]])
+
+    new_channels = ff.channel_labels() + ["UMAP1", "UMAP2", "Cluster"]
+    write_fcs(
+        filename=args.output,
+        chn_names=new_channels,
+        data=new_data
+    )
+
+    plt.figure(figsize=(6,6))
+    plt.scatter(umap_res[:,0], umap_res[:,1], c=km.labels_, cmap='tab10', s=1)
+    plt.xlabel("UMAP1")
+    plt.ylabel("UMAP2")
+    plt.title("UMAP colored by cluster")
+    plt.tight_layout()
+    plt.savefig(args.plot, dpi=150)
+
+if __name__ == "__main__":
+    main()
